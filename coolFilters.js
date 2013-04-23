cdd3e997e93f49558cf269fe1266c496f809707a
Ryan Endacott <rzeg24@gmail.com>
2013-04-23 00:20:39 -0500

    Added lots of filters to coolfilters

--- coolFilters.js
+++ coolFilters.js
@@ -1,3 +1,21 @@
+// This file contains many filters that you can use with the JavaScript Photofilter demo!
+// The demo is the index.html file in this folder.  
+// It is also currently hosted at http://mizzouacm.github.io/javascript-workshop/
+// Past each filter 
+// NOTE:  You may want to try to figure them out yourself!  It can be fun!
+
+// The current filters in this file are:
+// 1. Unique color filter
+// 2. Basic sepia filter
+// 3. Black and white filter
+// 4. Simple brightness filter
+// 5. Stripes effect
+// 6. Checkerboard effect
+// 7. Grid effect
+
+// See if you can implement them before checking the solution!
+
+// The filters:
 
 /*
 *  Unique Colors filter:
@@ -17,10 +35,85 @@
 var numColors = 256;
 var h = 255 / Math.pow(numColors, 1/3);
 function filter(pixels) {
-    for (var i = 0; i < pixels.length; i++) {
-        var p = pixels[i];
-        p.r = Math.floor(p.r / h) * h;
-        p.g = Math.floor(p.g / h) * h;
-        p.b = Math.floor(p.b / h) * h;
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    p.r = Math.floor(p.r / h) * h;
+    p.g = Math.floor(p.g / h) * h;
+    p.b = Math.floor(p.b / h) * h;
+  }
+}
+
+
+// Basic sepia filter
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    p.r = p.r * .8;
+    p.g = p.g * .5;
+    p.b = p.b * .2;
+  }
+}
+
+
+// Black and white filter
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    // Average all of the colors so they are all equal to make a shade of gray.
+    p.r = p.g = p.b = (p.r + p.g + p.b)/3;
+  }
+}
+
+
+// Simple brightness filter
+var delta = 20; // Change this to change bright/dim effect
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    p.r += delta;
+    p.g += delta;
+    p.b += delta;
+  }
+}
+
+
+// Stripes effect
+var stripeSize = 50;
+var helper = stripeSize * 2;
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    // Change to y to make horizontal stripes
+    if (p.x % helper > stripeSize) {
+      // Add your effect here
+      p.r = 255;
     }
+  }
+}
+
+// Checkerboard effect
+var squareSize = 50;
+var helper = squareSize * 2;
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    if ((p.x % helper > squareSize && p.y % helper > squareSize) || 
+      (p.x % helper < squareSize && p.y % helper < squareSize)) {
+      // Add your effect here
+      p.r = p.g = p.b = 255;
+    }
+  }
+}
+
+
+// Grid effect
+var gridSize = 50;
+function filter(pixels) {
+  for (var i = 0; i < pixels.length; i++) {
+    var p = pixels[i];
+    if (p.x % gridSize == 0 || p.y % gridSize == 0) {
+      // Add your effect here
+      p.r = p.g = p.b = 255;
+    }
+  }
 }
